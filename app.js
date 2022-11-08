04165b0b853dc348222cde81129a6516b7dfed04
Tatiana Maddix <tatianamaddix@gmail.com>
2022-11-08 18:48:52 -0500

    new push

---
+++ app.js
@@ -0,0 +1,85 @@
+function createParagraph(){
+var wordOne = document.getElementById('word1').value;
+var wordTwo = document.getElementById("word2").value;
+var wordThree= document.getElementById("word3").value;
+var wordFour = document.getElementById("word4").value;
+var wordFive = document.getElementById("word5").value;
+var wordSix = document.getElementById("word6").value;
+var wordSeven = document.getElementById("word7").value;
+var wordEight= document.getElementById("word8").value;
+var wordNine = document.getElementById("word9").value;
+var wordTen = document.getElementById("word10").value;
+var wordEleven = document.getElementById("word11").value;
+var wordTwelve = document.getElementById("word12").value;
+var wordThirteen = document.getElementById("word13").value;
+var wordFourteen = document.getElementById("word14").value;
+var wordFirteen = document.getElementById("word15").value;
+var wordSixteen = document.getElementById("word16").value;
+var wordSeventeen = document.getElementById("word17").value; 
+
+
+var paragraph =
+  "My cubicle is  " +
+  wordOne +
+  "It is the " +
+  wordTwo +
+  "cubicle in the office. I have a " +
+  wordThree +
+  "on my desk next to a(N) " +
+  wordFour +
+  " " +
+  wordFive +
+  "." +
+  "In my drawer, I also have a(n) " +
+  wordSix +
+  " " +
+  wordSeven +
+  "." +
+  "One time a coworker tried to " +
+  wordEight +
+  "a " +
+  wordNine +
+  " " +
+  "on my desk. I said to him, Hey how would you like it if I " +
+  wordTen +
+  "your" +
+  wordEleven +
+  "? I'll do it if you don't leave." +
+  " My one compliant about my cubicle is that it is " +
+  wordTwelve +
+  "." +
+  "I think everyone here at the office complains about this. We also complain that our cubicle is " +
+  wordThirteen +
+  "." +
+  " " +
+  " If we had money in our budget, my boss could purchase some " +
+  wordFourteen +
+  "to help alleviate this problem. Our boss doesn't understand. His office is the size of " +
+  wordFirteen +
+  "." +
+  " " +
+  "He has enough room in his office to put a " +
+  wordSixteen +
+  "and a" +
+  wordSeventeen +
+  "in there."
+
+document.getElementById("answer").innerHTML = paragraph;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+} 
